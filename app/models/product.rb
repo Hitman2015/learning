cb11f465f15f1d7c4f8932af1798a6713fe7d27a
Ruslan Zahar <666ruslan6614@gmail.com>
2022-03-29 18:52:19 +0300

    Add Validation for title length

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
 	validates :title, :description, :image_url, presence: true
 	validates :title, uniqueness: true
+	validates :title, length: { minimum: 10,
+															message: "Too short Title (at least 10 symbols)" }
 	validates :image_url, allow_blank: true, format: {
 							with: %r{\.(gif|jpg|png)\z}i,
 							message: 'must be a URL for GIF, JPG or PNG image.'
